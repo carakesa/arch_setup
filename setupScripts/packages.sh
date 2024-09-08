4226aef870b8d2f74bb7b2b4bb05df5c2cb5ecf1
carakesa <carakesa@gmail.com>
2024-09-07 22:02:57 -0400

    update package lists, modify own confs to carakesa.conf, update env and preferred apps

--- setupScripts/packages.sh
+++ setupScripts/packages.sh
@@ -10,9 +10,9 @@ EOF
 
 #!/bin/bash
 # Ensure yay is installed
-if ! command -v yay &> /dev/null; then
-    echo "yay is not installed. Installing yay..."
-    sudo pacman -S yay --noconfirm
+if ! command -v yay &>/dev/null; then
+  echo "yay is not installed. Installing yay..."
+  sudo pacman -S yay --noconfirm
 fi
 
 # -------------------------------------------------------
@@ -20,129 +20,130 @@ fi
 # -------------------------------------------------------
 
 packagesPacman=(
-    # General
-##    "kitty"
-    "wezterm"
-    "fastfetch"
-    "btop"
-    "ripgrep"
-##    "baobab"
-    "ncdu"
-    
-    # Communication
-    "discord"
-##    "telegram-desktop"
-    
-    # Utilities
-##    "gvfs"
-##    "w3m"
-##    "glow"
-##    "zathura"
-##    "zathura-pdf-poppler"
-    "okular"
-##    "texlive"
-##    "xclip"
-    "less"
-    
-    # File Managers
-##    "nautilus"
-##    "eog"
-    "unzip"
-    "pcmanfm"
-    
-    # Multimedia
-##    "obs-studio"
-    "mpv"
-##    "spotify-adblock"
-    "spotify"
-##    "zoom"
-    "ffmpeg"
-    "ffmpegthumbnailer"
-    "xwaylandvideobridge"
-##    "gimp"
-##    "libreoffice-fresh"
-    
-    # System Tools
-    "pacman-contrib"
-    "bluez"
-    "bluez-utils"
-    "wget"
-    "firefox"
-    "dunst"
-##    "starship"
-    "fish"
-    "fisher"
-    "fish-tide-git"
-    
-    # Additional Tools
-    "eza"
-    "python-pip"
-    "python-psutil"
-##    "python-rich"
-##    "python-click"
-    "python-pywal"
-##    "python-gobject"
-    "pavucontrol"
-##    "tumbler"
-    
-    # GNOME and GTK
-##    "colloid-icon-theme-git"
-##    "polkit-gnome"
-    "brightnessctl"
-##    "gum"
-    "man-pages"
-    "nm-connection-editor"
-    "xdg-user-dirs"
-    "xdg-desktop-portal-gtk"
-    "networkmanager"
-    "network-manager-applet"
-    "xarchiver"
-    "zip"
-    "fuse2"
-    "gtk4"
-    "libadwaita"
-    "xdg-desktop-portal"
-##    "qalculate-gtk"
-    "imagemagick"
-##    "guvcview"
-    "jq"
-    "rofi-wayland"
-    "blueman"
-    
-    # Fonts
-    "noto-fonts"
-    "noto-fonts-cjk"
-    "otf-font-awesome"
-    "ttf-fira-sans"
-    "ttf-firacode-nerd"
-    "ttf-recursive-nerd"
-    "ttf-cascadia-code-nerd"
-    "ttf-jetbrains-mono-nerd"
-    "figlet"
-    "fontpreview"
-    "ttf-iosevkaterm-nerd"
-    
-    # Hyprland Components
-    "hyprland"
-    "hyprpaper"
-    "hyprlock"
-    "hypridle"
-    "xdg-desktop-portal-hyprland"
-    
-    # Wayland Utilities
-    "waybar"
-    "grim"
-    "slurp"
-    "swappy"
-    "cliphist"
+  # General
+  ##    "kitty"
+  "wezterm"
+  "fastfetch"
+  "btop"
+  "ripgrep"
+  ##    "baobab"
+  "ncdu"
+
+  # Communication
+  "discord"
+  ##    "telegram-desktop"
+
+  # Utilities
+  ##    "gvfs"
+  ##    "w3m"
+  ##    "glow"
+  ##    "zathura"
+  ##    "zathura-pdf-poppler"
+  "okular"
+  ##    "texlive"
+  ##    "xclip"
+  "less"
+
+  # File Managers
+  ##    "nautilus"
+  ##    "eog"
+  "unzip"
+  "pcmanfm"
+
+  # Multimedia
+  ##    "obs-studio"
+  "mpv"
+  ##    "spotify-adblock"
+  ##    "zoom"
+  "ffmpeg"
+  "ffmpegthumbnailer"
+  "xwaylandvideobridge"
+  ##    "gimp"
+  ##    "libreoffice-fresh"
+
+  # System Tools
+  "pacman-contrib"
+  "bluez"
+  "bluez-utils"
+  "wget"
+  "firefox"
+  "dunst"
+  ##    "starship"
+  "fish"
+  "fisher"
+  "fish-tide-git"
+
+  # Additional Tools
+  "eza"
+  "python-pip"
+  "python-psutil"
+  ##    "python-rich"
+  ##    "python-click"
+  "python-pywal"
+  ##    "python-gobject"
+  "pavucontrol"
+  ##    "tumbler"
+
+  # GNOME and GTK
+  ##    "colloid-icon-theme-git"
+  ##    "polkit-gnome"
+  "brightnessctl"
+  ##    "gum"
+  "man-pages"
+  "nm-connection-editor"
+  "xdg-user-dirs"
+  "xdg-desktop-portal-gtk"
+  "networkmanager"
+  "network-manager-applet"
+  "xarchiver"
+  "zip"
+  "fuse2"
+  "gtk4"
+  "libadwaita"
+  "xdg-desktop-portal"
+  ##    "qalculate-gtk"
+  "imagemagick"
+  ##    "guvcview"
+  "jq"
+  "rofi-wayland"
+  "blueman"
+
+  # Fonts
+  "noto-fonts"
+  "noto-fonts-cjk"
+  "otf-font-awesome"
+  "ttf-fira-sans"
+  "ttf-firacode-nerd"
+  "ttf-recursive-nerd"
+  "ttf-cascadia-code-nerd"
+  "ttf-jetbrains-mono-nerd"
+  "figlet"
+  "ttf-iosevkaterm-nerd"
+
+  # Hyprland Components
+  "hyprland"
+  "hyprpaper"
+  "hyprlock"
+  "hypridle"
+  "xdg-desktop-portal-hyprland"
+
+  # Wayland Utilities
+  "waybar"
+  "grim"
+  "slurp"
+  "swappy"
+  "cliphist"
 )
 
 packagesYay=(
-    # Additional Tools
-    "wlogout"
-    "nwg-look"
-    "hyprshade"
-    "hyprshot"
+  # Additional Tools
+  "wlogout"
+  "nwg-look"
+  "hyprshade"
+  "hyprshot"
+  "fontpreview"
+  "trizen"
+  "spotify"
 )
 
 # Merge both arrays
@@ -153,17 +154,17 @@ allPackages=("${packagesPacman[@]}" "${packagesYay[@]}")
 # -------------------------------------------------------
 
 for package in "${allPackages[@]}"; do
-    if yay -Qi "$package" &> /dev/null; then
-        echo "$package is already installed. Skipping..."
+  if yay -Qi "$package" &>/dev/null; then
+    echo "$package is already installed. Skipping..."
+  else
+    if [[ " ${packagesPacman[*]} " =~ " ${package} " ]]; then
+      echo "Installing $package from pacman..."
+      sudo pacman -S "$package" --noconfirm
     else
-        if [[ " ${packagesPacman[*]} " =~ " ${package} " ]]; then
-            echo "Installing $package from pacman..."
-            sudo pacman -S "$package" --noconfirm
-        else
-            echo "Installing $package from yay..."
-            yay -S "$package" --noconfirm
-        fi
+      echo "Installing $package from yay..."
+      yay -S "$package" --noconfirm
     fi
+  fi
 done
 
 echo "All packages installed!"
